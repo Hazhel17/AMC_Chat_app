024b0c56c916e6d6832c17a5cf41839f1e334fff
hazel <hazelannconvicto@gmail.com>
2026-01-20 16:57:28 +0800

    lab 2 Flutter chat application

--- chat_ui_lab/lib/main.dart
+++ chat_ui_lab/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'screens/chat_screen.dart';  // ← Points to FULL chat screen
+import 'screens/chat_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';// ← Points to FULL chat screen
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try{
+    await dotenv.load(fileName: ".env");
+  }
+  catch (e) {
+    throw Exception('Error loading .env file: $e');
+  }
   runApp(ChatApp());
 }
 
--- chat_ui_lab/lib/services/gemini_service.dart
+++ chat_ui_lab/lib/services/gemini_service.dart
@@ -1,14 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class GeminiService {
-  static const String apiKey = 'AIzaSyC0c8TfyF9OCkArRBhWe2_70d9peSI_3K4';  // ← Replace with your actual API key!
-  static const String apiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent';
+  static String? get _apiKey => dotenv.env['API_KEY'];
+  static String? get _baseUrl => dotenv.env['BASE_URL'];
 
   static Future<String> sendMessage(String message) async {
+    if (_baseUrl == null || _apiKey == null) {
+      return 'Error: BASE_URL or API_KEY is missing in .env file.';
+    }
+
     try {
       final response = await http.post(
-        Uri.parse('$apiUrl?key=$apiKey'),
+        Uri.parse('$_baseUrl?key=$_apiKey'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'contents': [
@@ -21,7 +26,13 @@ class GeminiService {
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        return data['candidates'][0]['content']['parts'][0]['text'];
+
+        if (data['candidates'] != null &&
+            (data['candidates'] as List).isNotEmpty) {
+          return data['candidates'][0]['content']['parts'][0]['text'];
+        } else {
+          return 'Error: No response. (The query might have been blocked by safety filters)';
+        }
       } else {
         return 'Error: ${response.statusCode} - ${response.body}';
       }
